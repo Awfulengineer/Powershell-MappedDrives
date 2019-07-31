4999e3dbcfa86a0f830505e504442e4058c64df5
Awfulengineer <53479183+Awfulengineer@users.noreply.github.com>
2019-07-31 14:04:58 +0100

    Add files via upload

---
+++ GetMappedDrives.ps1
@@ -0,0 +1,9 @@
+﻿Get-ChildItem "HKCU:Network\" |
+    ForEach-Object {
+        [PsCustomObject]@{
+            User = whoami
+            Computer = $env:computername
+            DriveLetter = $_.PSChildName
+            RemotePath = (Get-ItemProperty $_.PSPath).RemotePath
+         }
+    } | Export-Csv '\\BE-DC01\MappedDrives\MappedDrives.csv' -Append -NoTypeInformation
